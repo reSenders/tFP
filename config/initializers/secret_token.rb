6e06a68e5ef85bfb0e5682e9f82f3b08d332bedf
Thomas Fayon <thomas@neuilly.net>
2014-05-19 15:36:25 +0200

    tFP initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TFP::Application.config.secret_key_base = '2f4f18b9aa4c3330461e6582340f8ce63db8e558d767d155abf4bf8e695312db5808fa9ef29a46b753b2951b83bc90938a86422513c6629f9eec50ded89fb46f'
