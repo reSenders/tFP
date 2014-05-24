d17a29b7d90bf52c79fd52f3a7fe96d8de1459fb
Thomas Fayon <thomas@neuilly.net>
2014-05-24 15:11:27 +0200

    BF

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,4 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	:storage => :s3,
-  :bucket => 'thefaradayproject',
-  :s3_credentials => {:access_key_id => ENV['AKIAIDJD2EO45ITGJ3MQ'], :secret_access_key => ENV['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']}
 end
