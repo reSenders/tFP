bfbba97aa31e72489362a90e2c5a79d5bcbe0110
Thomas Fayon <thomas@neuilly.net>
2014-05-24 15:03:37 +0200

    Bug fixing

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,6 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	:storage => :s3,
-  :bucket => '******',
+  :bucket => 'thefaradayproject',
   :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 end
