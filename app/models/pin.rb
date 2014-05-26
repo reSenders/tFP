d3af6600962411eee419b7460b87c62e11ba2939
Thomas Fayon <thomas@neuilly.net>
2014-05-26 16:34:46 +0200

    One more time add Amazon S3 for Paperclip uploads to Heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,9 +2,4 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	:storage => :s3,
-  :bucket  => ENV['theFaradayProject']
-  validates :image, presence: true
-  validates :description, presence: true
-end
 end
